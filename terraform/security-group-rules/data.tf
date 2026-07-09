5d6a7102171d412c0aeff3aaa53e8a48af699447
megha <megha@gmail.com>
2026-07-09 20:21:29 +0530

    04-07

---
+++ terraform/security-group-rules/data.tf
@@ -0,0 +1,31 @@
+data "aws_ssm_parameter" "catalogue_sg_id" {
+    name = "${local.common_name}_catalogue_sg_id"
+}
+
+data "aws_ssm_parameter" "user_sg_id" {
+    name = "${local.common_name}_user_sg_id"
+}
+
+data "aws_ssm_parameter" "cart_sg_id" {
+    name = "${local.common_name}_cart_sg_id"
+}
+
+data "aws_ssm_parameter" "shipping_sg_id" {
+    name = "${local.common_name}_shipping_sg_id"
+}
+
+data "aws_ssm_parameter" "payment_sg_id" {
+    name = "${local.common_name}_payment_sg_id"
+}
+
+data "aws_ssm_parameter" "bastion_sg_id" {
+    name = "${local.common_name}_bastion_sg_id"
+}
+
+data "aws_ssm_parameter" "frontend_alb_sg_id" {
+    name = "${local.common_name}_frontend_alb_sg_id"
+}
+
+data "aws_ssm_parameter" "backend_alb_sg_id" {
+    name = "${local.common_name}_backend_alb_sg_id"
+}
