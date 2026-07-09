52b78595627583cd2fd35de08d62bfd27bac747c
megha <megha@gmail.com>
2026-07-09 19:30:56 +0530

    04-07

--- terraform/vpc/provider.tf
+++ terraform/vpc/provider.tf
@@ -8,7 +8,7 @@ terraform {
   backend "s3" {
     bucket = "dcnh"
     region = "us-east-1"
-    key = "08-07-statefiles"
+    key = "08-07-statefiles-VPC"
     use_lockfile = true
     encrypt = true
   }
