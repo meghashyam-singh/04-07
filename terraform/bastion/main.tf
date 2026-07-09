6ae1a30248ab2886fbb1f654e50da8035983ed3f
megha <megha@gmail.com>
2026-07-09 23:13:55 +0530

    04-07

--- terraform/bastion/main.tf
+++ terraform/bastion/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "bastion" {
     ami = data.aws_ami.ami_id.id
     instance_type = var.environment == "dev" ? "t3.micro" : "t3.medium"
-    subnet_id = split(",", data.aws_ssm_parameter.public_subnet_ids)[0]
+    subnet_id = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
     vpc_security_group_ids = [ data.aws_ssm_parameter.bastion_sg_id.value ]
     tags = {
         Name = "${local.common_name}_bastion"
