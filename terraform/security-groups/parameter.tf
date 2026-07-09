bde4357086ec498810db201c78dd04e7c392f6d9
megha <megha@gmail.com>
2026-07-09 19:35:22 +0530

    04-07

--- terraform/security-groups/parameter.tf
+++ terraform/security-groups/parameter.tf
@@ -2,5 +2,5 @@ resource "aws_ssm_parameter" "roboshop_sg" {
     count = length(var.sg_names)
     name = "${local.common_name}_${var.sg_names[count.idex]}_sg_id"
     type = "String"
-    value = aws.security_group.roboshop_sg[count.index].id
+    value = aws_security_group.roboshop_sg[count.index].id
 }
